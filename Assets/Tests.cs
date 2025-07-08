aa6e5da7d0d253e1770a83ce6c27ed2125299281
smallmoon2 <cyros12@naver.com>
2025-07-08 16:38:39 +0900

    test

--- Assets/Tests.cs
+++ Assets/Tests.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Tests : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+
     {
         
     }
